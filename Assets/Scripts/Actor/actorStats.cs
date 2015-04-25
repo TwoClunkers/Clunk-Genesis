e0b34c738a9bda6c436b283aa3209cd34da54a20
RossMcConeghy <wabikun@gmail.com>
2015-04-25 14:26:54 -0700

    Actor stats & buffs very very beginning
    just a starting point for this stuff.

---
+++ Assets/Scripts/Actor/actorStats.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class actorStats : MonoBehaviour {
+	//current value of main stats
+	float stability; //health
+	float energy; //energy
+	float fluid; //speed
+
+	//base stats
+	float durability;
+	float capacitance;
+	float volume;
+	float aptitude;
+	float precision;
+
+	//MAX value of main stats
+	float stabilityMax; //health
+	float energyMax; //energy
+	float fluidMax; //speed
+	
+	//Gimp = exact amount of stat reduction 1:1
+	float stabilityGimp;
+	float energyGimp;
+	float fluidGimp;
+
+	void Start() {
+		//TODO: get actor template index & set initial stat values
+
+	}
+
+	void calculateStats() {
+		//TODO: poll all equipment & currently running buffs.
+	}
+
+}
---
+++ Assets/Scripts/Actor/buffs.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum buffTypes{
+	additive, 
+	multiply, 
+	func
+}
+
+public class buffs : MonoBehaviour {
+	buffTypes type;
+	ArrayList stats;
+	ArrayList amounts;
+	float duration;
+	
+	void FixedUpdate () {
+		switch(type){
+		case buffTypes.additive:
+			//TODO: add amount to specified stat(s)
+			break;
+		case buffTypes.multiply:
+			//TODO: multiply specified stat(s) by amount
+			break;
+		case buffTypes.func:
+			//TODO: call function
+			break;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+}
